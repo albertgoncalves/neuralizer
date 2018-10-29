5bb78c0c4de1533711b31ee09b1c80a50f00fe44
albertgoncalves <goncalves.albert@gmail.com>
2018-10-28 21:41:26 -0400

    First commit.

---
+++ graphics/graphics.js
@@ -0,0 +1,45 @@
+const random      = ()          => Math.random();
+const rectId      = (x, y)      => `grid-${x}-${y}`;
+const range       = (limit)     => [...Array(limit).keys()];
+const arrayToStr  = (array)     => array.map((x) => x.toString());
+const arrayToHsl  = ([h, s, l]) => `hsl(${h}, ${s}%, ${l}%)`;
+const randBetween = (min, max)  => Math.floor(random() * (max - min)) + min;
+const randomHsl   = () => {
+    const h = randBetween( 0, 359);
+    const s = randBetween(50, 100);
+    const l = randBetween(40,  80);
+    return arrayToHsl(arrayToStr([h, s, l]));
+};
+
+const createSvg = (containerId, svgShape, attributes) => {
+    const newSvg = document.createElementNS( "http://www.w3.org/2000/svg"
+                                           , svgShape
+                                           );
+    attributes.forEach(([prop, value]) => newSvg.setAttribute(prop, value));
+    document.getElementById(containerId).appendChild(newSvg);
+};
+
+const createSquare = (containerId, unit) => (x, y, color, id) => {
+    const squareAttributes = [ ["id"    , id   ]
+                             , ["x"     , x    ]
+                             , ["y"     , y    ]
+                             , ["width" , unit ]
+                             , ["height", unit ]
+                             , ["fill"  , color]
+                             ];
+    createSvg(containerId, "rect", squareAttributes);
+};
+
+const changeColor = (color, id) => {
+    document.getElementById(id).style.fill = color;
+};
+
+const drawGrid = (squareInit, xEdges, yEdges, colorA, colorB, colorFun) => {
+    xEdges.forEach(
+        (x) => yEdges.forEach(
+            (y) => {
+                squareInit(x, y, colorFun(x, y), rectId(x, y));
+            }
+        )
+    );
+};
---
+++ graphics/main.js
@@ -0,0 +1,17 @@
+// const blue        = "hsl(205, 85%, 65%)";
+// const red         = "hsl(  5, 95%, 75%)";
+const colorA      = randomHsl();
+const colorB      = randomHsl();
+const colorC      = randomHsl();
+const len         = document.getElementById("figure").clientWidth;
+const containerId = "axis";
+const res         = Math.pow(2, 4);
+const unit        = len / res;
+const edges       = range(res).map((x) => x * unit);
+const squareInit  = createSquare(containerId, unit);
+const colorFun    = (x, y) => (x + y) >= len ? colorA
+                                             : colorB;
+
+drawGrid(squareInit, edges, edges, colorA, colorB, colorFun);
+
+changeColor(colorC, rectId(0, 0));
