aebc6efbcb9fac499e64690d160f3389bca2211e
albertgoncalves <goncalves.albert@gmail.com>
2018-11-07 00:00:25 -0500

    Clicks.

--- graphics/main.js
+++ graphics/main.js
@@ -3,9 +3,10 @@
 const colorA      = randomHsl();
 const colorB      = randomHsl();
 const colorC      = randomHsl();
+const checkGridId = (id) => id.indexOf("grid-") !== -1;
 const len         = document.getElementById("figure").clientWidth;
 const containerId = "axis";
-const res         = Math.pow(2, 5);
+const res         = Math.pow(2, 4);
 const unit        = len / res;
 const edges       = range(res).map((x) => x * unit);
 const squareInit  = createSquare(containerId, unit);
@@ -14,6 +15,18 @@ const colorFun    = (x, y) => (x + y) >= len ? colorA
 const grid        = iterGrid(edges, edges);
 const drawGrid    = (x, y) => squareInit(x, y, colorFun(x, y), rectId(x, y));
 const randomGrid  = (x, y) => changeColor(randomHsl(), rectId(x, y));
+const idToCoords  = (gridId) => gridId.match(/\d+/g).map(Number);
+const clickAction = (gridId) => {
+    changeColor(randomHsl(), gridId);
+    console.log(idToCoords(gridId));
+};
 
 grid(drawGrid);
 // grid(randomGrid);
+
+// window.onclick = (e) => console.log(e.target.id);
+window.onclick = (e) => {
+    const gridId      = e.target.id;
+    const _ = checkGridId(gridId) ? clickAction(gridId)
+                                  : null;
+};
