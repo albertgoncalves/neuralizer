f214bb60849e305de8429f2e4715ec72d8f89a86
albertgoncalves <goncalves.albert@gmail.com>
2018-11-16 19:01:34 -0500

    Improvements.

--- shell.nix
+++ shell.nix
@@ -5,14 +5,33 @@ with pkgs; mkShell {
 
     buildInputs = [ htmlTidy
                     nodejs-8_x
+                    fzf
                   ];
 
     shellHook = ''
-        if [ ! -e node_modules/.bin/jshint ]
-        then
+        if [ ! -e node_modules/.bin/jshint ]; then
             npm install --save-dev jshint
         fi
 
         export PATH="$PWD/node_modules/.bin/:$PATH"
+
+        fzfh()    { find . | fzf; }
+        strcd()   { cd "$(dirname $1)"; }
+        withfzf() {
+            local h
+            h="$(fzf --exact)"
+            if (( $? == 0 )); then
+                $1 $h
+            fi
+        }
+
+        alias  cdfzf="withfzf strcd"
+        alias jshfzf="withfzf jshint"
+        alias runfzf="withfzf node"
+        alias tidfzf="withfzf tidy"
+        alias vimfzf="withfzf vim"
+
+        export -f fzfh
+        export -f withfzf
     '';
 }
