ae537877080d132bf4b3831d0f379fd418432852
albertgoncalves <goncalves.albert@gmail.com>
2019-07-06 03:35:49 -0400

    rename

--- shell.nix
+++ shell.nix
@@ -1,21 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
-
 with pkgs; mkShell {
-    name = "javascript";
-
-    buildInputs = [ htmlTidy
-                    nodejs-8_x
-                    fzf
-                    tmux
-                  ];
-
+    name = "neuralizer";
+    buildInputs = [
+        htmlTidy
+        nodejs-8_x
+        fzf
+        tmux
+    ];
     shellHook = ''
         if [ ! -e node_modules/.bin/jshint ]; then
             npm install --save-dev jshint
         fi
-
         export PATH="$PWD/node_modules/.bin/:$PATH"
-
         fzfh() { find . | fzf; }
         strcd() { cd "$(dirname $1)"; }
         withfzf() {
@@ -25,13 +21,11 @@ with pkgs; mkShell {
                 $1 $h
             fi
         }
-
         alias cdfzf="withfzf strcd"
         alias jshfzf="withfzf jshint"
         alias runfzf="withfzf node"
         alias tidfzf="withfzf tidy"
         alias vimfzf="withfzf vim"
-
         export -f fzfh
         export -f withfzf
     '';
