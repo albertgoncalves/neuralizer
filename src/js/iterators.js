cd4ecfaea99ee225726476d5b06c388ec4857d3d
albertgoncalves <goncalves.albert@gmail.com>
2019-07-20 17:14:22 -0400

    lite overhaul to lint/fmt workflow

--- src/js/iterators.js
+++ src/js/iterators.js
@@ -37,7 +37,7 @@ function sum(xs) {
 function findAll(xs, x) {
     var n = xs.length;
     var ys = [];
-    for (i = 0; i < n; i++) {
+    for (var i = 0; i < n; i++) {
         if (xs[i] === x) {
             ys.push(i);
         }
